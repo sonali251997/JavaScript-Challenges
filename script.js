db73bb9147b7dbd6028569fdfa6a95bbe2f05780
Sonali Prajapati <85176870+sonali251997@users.noreply.github.com>
2023-04-26 18:11:14 +0530

    new changes

--- script.js
+++ script.js
@@ -215,3 +215,16 @@ const duplicateCount = (text) => {
 console.log(duplicateCount("abA11"));
 console.log(duplicateCount("aabbcde"));
 console.log(duplicateCount("aabBcdeE"));
+
+// Question 18 Find a second largest number in array
+function secondLargest(arr) {
+  arr.sort();
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] != arr[i - 1]) {
+      return arr[i];
+    }
+  }
+}
+
+arr = [4, 2, 1, 3, 5];
+console.log(`The second largest element is: ${secondLargest(arr)}`);
